3ec12fff8173fe03b32d7dbf3b443d3c73ed0333
Samraj Subramaniam <samrajs@gmail.com>
2025-02-07 11:08:27 +0100

    First commit of RAG application

---
+++ ingestion.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain_pinecone import PineconeVectorStore
+from langchain_core.prompts import PromptTemplate
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+
+load_dotenv()
+
+if __name__ == '__main__':
+    print("Ingesting...")
+    loader = TextLoader("./mediumblog1.txt")
+    document = loader.load()
+
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OllamaEmbeddings(model="znbang/bge:small-en-v1.5-q8_0")
+
+    print("ingesting...")
+
+    vectorstore = FAISS.from_documents(
+                                        texts,
+                                        embedding=embeddings,)
+
+
+    vectorstore.save_local("faiss_index_react")
+
+    print("done")
---
+++ rag.py
@@ -0,0 +1,35 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    print(" Retrieving...")
+
+    embeddings = OllamaEmbeddings(model="znbang/bge:small-en-v1.5-q8_0")
+    llm = ChatOllama(model="llama3.2", temperature=0)
+
+    query = "what is Pinecone in machine learning?"
+
+    vectorstore = FAISS.load_local("huggingface_index_react", embeddings=embeddings, allow_dangerous_deserialization=True) 
+
+    print("Loaded vector store")
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    retrival_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+
+    result = retrival_chain.invoke(input={"input": query})
+
+    print(result['answer'])
